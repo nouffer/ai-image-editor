931a7a535aee6f95a07a9f99c62ba86275654a06
nouffer <nouffer@gmail.com>
2025-10-18 12:28:04 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="text-5xl font-extrabold tracking-tight text-gray-600 sm:text-[5rem]">
+      Home
+    </div>
+  );
+}
